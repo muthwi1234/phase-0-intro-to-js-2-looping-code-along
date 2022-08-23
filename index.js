e121f05d0d70463f1ab849aefc2d7bd945329f6d
Peter <peterkilungya1@gmail.com>
2022-08-23 11:22:44 +0300

    assignment completeed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const writeCards = (array = ["Guadalupe", "Ollie", "Aki"])=>{
+    const cardArray = [];for (const a of array){
+        cardArray.push(`Thank you, ${a}, for the wonderful surprise gift!`);
+}
+    return cardArray;
+}
+function countDown(){
+    let countdown = 10;
+    while (countdown > 0){
+        console.log(countdown--);
+    }
+    console.log(countdown);
+}
